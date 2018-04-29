0b9689c4baea9d047dcd22e12316c71194ebfe77
Bsniv <bsniv10@gmail.com>
2018-04-29 16:07:55 +0300

    HashServices added

---
+++ wsep182/Domain/HashArchive.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+
+namespace wsep182.Domain
+{
+    public class HashArchive
+    {
+        private static HashArchive instance;
+        private Dictionary<string, User> hashes;
+        private HashArchive()
+        {
+            hashes = new Dictionary<string,User>();
+        }
+        public static HashArchive getInstance()
+        {
+            if (instance == null)
+                instance = new HashArchive();
+            return instance;
+        }
+
+        public static void restartInstance()
+        {
+            instance = new HashArchive();
+        }
+        public Boolean configureUser(string hash, User user)
+        {
+            if (!hashes.ContainsKey(hash))
+            {
+                hashes.Add(hash, user);
+                return true;
+            }
+            return false;
+        }
+        
+        public string generateID()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+
+
+        public User getUserByHash(string hash)
+        {
+            if (hashes.ContainsKey(hash))
+            {
+                return hashes[hash];
+            }
+            return null;
+        }
+
+
+    }
+}
---
+++ wsep182/services/hashServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wsep182.Domain;
+
+namespace wsep182.services
+{
+    class hashServices
+    {
+        public Boolean configureUser(string hash, User user)
+        {
+            return HashArchive.getInstance().configureUser(hash, user);
+        }
+
+        public string generateID()
+        {
+            return HashArchive.getInstance().generateID();
+        }
+
+
+        public User getUserByHash(string hash)
+        {
+            return HashArchive.getInstance().getUserByHash(hash);
+        }
+    }
+}
