803e26f32b817415a96f8f84030f9a7edc6a735f
tzachi46 <tzachiabo@gmail.com>
2018-05-01 10:42:34 +0300

    gui fixes

---
+++ WebServices/Controllers/PagesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebServices.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Pages
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult temp()
+        {
+            return View();
+        }
+    }
+}
