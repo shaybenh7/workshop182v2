a487ec5639b155d87bcc0246b2bf6f756216ef3a
Bsniv <bsniv10@gmail.com>
2018-05-12 09:24:37 +0300

    Testing environment is more stable

--- SeleniumTests/UnitTest1.cs
+++ SeleniumTests/UnitTest1.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+using System.Windows.Forms;
+using System.Threading;
 
 namespace SeleniumTests
 {
@@ -11,11 +13,11 @@ namespace SeleniumTests
     {
         public static String URL = "http://localhost:53416/";
         IWebDriver driver = new ChromeDriver("./");
-
+        private int sleepTime = 500;
         [TestInitialize]
         public void Initialize()
         {
-            //Navigate to Execute automation demo page
+            driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(URL);
             Console.WriteLine("Opened URL");
         }
@@ -26,18 +28,24 @@ namespace SeleniumTests
         {
             IWebElement fillDB = driver.FindElement(By.Id("fillDB"));
             fillDB.Click();
+            Thread.Sleep(sleepTime);
             IWebElement AllProducts = driver.FindElement(By.Id("AllProductsLink"));
             AllProducts.Click();
+            Thread.Sleep(sleepTime);
             IWebElement sale1 = driver.FindElement(By.Id("viewSale0"));
             sale1.Click();
+            Thread.Sleep(sleepTime);
             IWebElement submitViewInstantSale = driver.FindElement(By.Id("submit"));
             submitViewInstantSale.Click();
+            Thread.Sleep(sleepTime);
             IAlert alert = driver.SwitchTo().Alert();
             string alertText = alert.Text;
             Assert.IsTrue(alertText.Contains("Product was added successfully!"));
             alert.Accept();
+            Thread.Sleep(sleepTime);
             IWebElement shoppingCartIcon = driver.FindElement(By.Id("shoppingCartIcon"));
             shoppingCartIcon.Click();
+            Thread.Sleep(sleepTime);
             IWebElement productInCart = driver.FindElement(By.Id("productName0"));
             Assert.IsTrue(productInCart.Text.Equals("cola"));
         }
